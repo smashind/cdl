8775563da42e50f1048443138c252b7b1e0212e4
Danny Baumann <danthemanvt@gmail.com>
2015-11-02 19:55:37 -0800

    Update how state picker widget works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,16 @@ class PagesController < ApplicationController
 	skip_before_action :verify_authenticity_token, only: [:success]
 	layout false, only: [:cdl_optin, :cdl_practice_test, :cdl_thank_you]
 
-  def cdl_option
+  def cdl_study_guides_and_practice_tests
+    @states = [ "Alaska","Alabama","Arkansas","Arizona","California","Colorado","Connecticut","District of Columbia",
+                "Delaware","Florida","Georgia","Hawaii","Iowa","Idaho","Illinois","Indiana","Kansas","Kentucky","Louisiana",
+                "Massachusetts","Maryland","Maine","Michigan","Minnesota","Missouri","Mississippi","Montana","North Carolina",
+                "North Dakota","Nebraska","New Hampshire","New Jersey","New Mexico","Nevada","New York","Ohio","Oklahoma",
+                "Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Virginia",
+                "Vermont","Washington","Wisconsin","West Virginia","Wyoming"]
+  end
+
+  def cdl_optin
   end
 
   def cdl_practice_test
