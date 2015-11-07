1767331b5f416cd75c4f9b01c04a5339803c45f7
Danny Baumann <danthemanvt@gmail.com>
2015-11-06 20:07:46 -0800

    Another paypal test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) << :name
   end
 
+  def after_sign_up_path_for(resource)
+    "http://www.cdlstudyguidehq.com/users/sign_in"
+  end
+
   def after_sign_in_path_for(resource)
-  	"http://www.cdlstudyguidehq.com/dashboard"
+  	dashboard_path
 	end
 end
