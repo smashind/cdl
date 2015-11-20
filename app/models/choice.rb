5d3b3138967f4e4e9ef5230c5ca9c750577e9782
Danny Baumann <danthemanvt@gmail.com>
2015-11-20 04:51:25 -0800

    Fix test editing and update product images

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,3 +1,4 @@
 class Choice < ActiveRecord::Base
   belongs_to :question
+  default_scope { order('id ASC') }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@ class Question < ActiveRecord::Base
   validates_presence_of :text
 	has_many :choices, dependent: :destroy
   accepts_nested_attributes_for :choices, reject_if: lambda { |a| a[:answer].blank? }, allow_destroy: true
+  default_scope { order('id ASC') }
 
   def next
   	self.class.where("test_id = ?", self.test).where("id > ?", id).first
