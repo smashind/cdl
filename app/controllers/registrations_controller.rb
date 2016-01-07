77707baaa7ac6fed86a26a5e1620f7816ec5045e
Danny Baumann <danthemanvt@gmail.com>
2016-01-06 17:08:46 -0800

    Update edit profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   def after_sign_up_path_for(resource)
     "http://www.cdlstudyguidehq.com/users/sign_in"
   end
